621f0e9dc3f60fb440abf53dfd7e5a61b07b091d
grade <andigo74@gmail.com>
2025-01-31 18:06:39 +0200

    Area triangle heron

---
+++ pythonProject 1/triangle.py
@@ -0,0 +1,13 @@
+import math
+
+
+def area(a,b,c):
+    p=(a+b+c)/2
+    s=math.sqrt(p*(p-a)*(p-b)*(p-c))
+    return s
+
+
+a=float(input('a='))
+b=float(input('b='))
+c=float(input('c='))
+print(area(a,b,c))
